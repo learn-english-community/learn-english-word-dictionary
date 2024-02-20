dc870ca8f21d2bbf2af9a06ce988f79a734bdde1
christolis <work@chris-sdogkos.com>
2024-02-21 00:45:50 +0200

    feat(consul): first working version

--- server.py
+++ server.py
@@ -3,10 +3,40 @@ from wiktionaryparser import WiktionaryParser
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
-import re
 from constants import *
+import consulate
+import threading
+import time
 
+SERVER_PORT = 8081
+APP_NAME = "word-wiktionary"
+CONSUL_PORT = 8500
+SERVER_IP = "localhost"
 parser = WiktionaryParser()
+
+# Register microservice to Consul agent
+consul = consulate.Consul()
+consul.agent.service.register(APP_NAME, port=SERVER_PORT, tags=['master'], ttl='10s')
+
+
+def send_consul_heartbeat(ip: str, port: int):
+  """Heartbeat for keeping the service alive"""
+  while True:
+    # Make a PUT request to update TTL
+    try:
+      response = requests.put(f'http://{ip}:{port}/v1/agent/check/pass/service:{APP_NAME}')
+
+      if response.status_code != 200:
+        print(f"Failed to send heartbeat: {response.text}")
+    except Exception as e:
+      print(f"Error sending heartbeat: {str(e)}")
+
+    time.sleep(5)  # Send heartbeat every 5 seconds
+
+
+# Start heartbeat thread
+heartbeat_thread = threading.Thread(target=send_consul_heartbeat, args=(SERVER_IP, CONSUL_PORT), daemon=True)
+heartbeat_thread.start()
 
 
 def fetch_word(word):
@@ -46,4 +76,4 @@ def server():
 
 app = server()
 if __name__ == "__main__":
-  app.run()
+  app.run(port=SERVER_PORT)
